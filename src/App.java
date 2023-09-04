f6ecf62d50f80636d8fae1c3676b2022befdaa7c
Aymeri TOURNEUR <aymeri.tourneur.etu@univ-lille.fr>
2023-09-04 16:09:08 +0200

    ajout de la classe ville

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Ville ville = new Ville("Test");
+        ville.setMoney(100);
+        System.out.println(ville);
     }
 }
