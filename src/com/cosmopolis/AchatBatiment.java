421a6151382cea3ffcb3e791927bfeecd884d26e
Aymeri TOURNEUR <aymeri.tourneur.etu@univ-lille.fr>
2023-09-05 09:04:36 +0200

    ajout de la fusée

---
+++ src/com/cosmopolis/AchatBatiment.java
@@ -0,0 +1,15 @@
+package com.cosmopolis;
+
+public class AchatBatiment {
+    
+    public static boolean acheterBatiment(Ville ville, int id) {
+        switch (id) {
+            case 1:
+                if(ville.getMoney() > )
+            
+            default:
+                break;
+        }
+
+    }
+}
