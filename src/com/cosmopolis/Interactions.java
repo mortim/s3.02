2138e5f47da48d011079cb03d8770d5e7cb7d371
Mathis Decoster <mathis.decoster.etu@univ-lille.fr>
2023-09-05 08:41:24 +0200

    interactions

---
+++ src/com/cosmopolis/Interactions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Interactions {
+    
+    private Scanner stringScanner;
+    private String inputString;
+
+    public int voirInput(){
+        stringScanner = new Scanner(System.in);
+        inputString = stringScanner.next();
+        if(inputString.equals("1")){
+            return 1;
+        } else if(inputString.equals("2")){
+            return 2;
+        }else if(inputString.equals("3")){
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+
+}
