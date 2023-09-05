ad82e4d13d0ef43f6b86c88500ebf85e6eb837f5
Mathis Decoster <mathis.decoster.etu@univ-lille.fr>
2023-09-05 08:50:00 +0200

    description interactions

--- src/com/cosmopolis/Interactions.java
+++ src/com/cosmopolis/Interactions.java
@@ -1,3 +1,4 @@
+package com.cosmopolis;
 import java.util.Scanner;
 
 public class Interactions {
@@ -5,6 +6,9 @@ public class Interactions {
     private Scanner stringScanner;
     private String inputString;
 
+    /**
+     * @return Renvoie un int en fonction de la saisie de l'utilisateur.
+     */
     public int voirInput(){
         stringScanner = new Scanner(System.in);
         inputString = stringScanner.next();
