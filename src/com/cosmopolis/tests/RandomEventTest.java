caa39ccbab90e834a99981b825b2e250581ea36b
Aymeri TOURNEUR <aymeri.tourneur.etu@univ-lille.fr>
2023-09-06 08:49:48 +0200

    suppresion d'un test et refactor

--- src/com/cosmopolis/tests/RandomEventTest.java
+++ src/com/cosmopolis/tests/RandomEventTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import com.cosmopolis.Ville;
-import com.cosmopolis.RandomEvent;
+import com.cosmopolis.evenements.RandomEvent;
 
 public class RandomEventTest {
     
     public RandomEvent r;
     public Ville v;
 
-    @BeforeEach
-    void init(){
-        v = new Ville("test nom");
-        v.setResidents(15);
-        r = new RandomEvent(15, "message", v);
-    }
+    // @BeforeEach
+    // void init(){
+    //     v = new Ville("test nom");
+    //     v.setResidents(15);
+    //     r = new RandomEvent(15, "message", v);
+    // }
 
-    @Test
-    void testGet(){
-        assertTrue(15 == r.getHabitants());
-    }
+    // @Test
+    // void testGet(){
+    //     assertTrue(15 == r.getHabitants());
+    // }
 
-    @Test
-    void testCreateEvent(){
-        r.createEvent(1.4,-1.5, "test message1");
-        assertTrue(15 < r.getVille().getResidents());
-    }
+    // @Test
+    // void testCreateEvent(){
+    //     r.createEvent(1.4,-1.5, "test message1");
+    //     assertTrue(15 < r.getVille().getResidents());
+    // }
 }
