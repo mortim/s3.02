a151a309bd06c50b65ed2ef61d025713c5164546
Aymeri TOURNEUR <aymeri.tourneur.etu@univ-lille.fr>
2023-09-07 08:25:20 +0200

    ajout d'informations dans l'aide

--- src/com/cosmopolis/interfaces/Click.java
+++ src/com/cosmopolis/interfaces/Click.java
@@ -11,9 +11,12 @@ public class Click extends Fenetre {
         println("║ Votre objectif est de construire la meilleure ville et d'envoyer une fusée.                             ║");
         println("║                                                                                                         ║");
         println("║ Tutoriel:                                                                                               ║");
-        println("║ Utilisez les flèches directionnelles pour jouer, ils vous permettent de naviguer entre les onglets.     ║");
-        println("║ Et les touches du pavé numérique vous permettent d'acheter des bâtiments.                               ║");
-        println("║ Appuyez sur [Q/q] pour quitter le jeu.                                                                  ║");
+        println("║ °-Utilisez les flèches directionnelles pour jouer, ils vous permettent de naviguer entre les onglets.   ║");
+        println("║   Et les touches du pavé numérique vous permettent d'acheter des bâtiments.                             ║");
+        println("║ °-1000 points de recherche sont nécessaires pour envoyer une fusée dans l'espace.                       ║");
+        println("║ °-Les batiments spéciaux (Caserne de pompier,Abris etc..) permmettent de réduire les risques            ║");
+        println("║   d'évènements indésirables (Séismes, Tempêtes etc..).                                                  ║");
+        println("║ °-Appuyez sur [Q/q] pour quitter le jeu.                                                                ║");
         println("╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
         println("");
         Utils.printTxt("src/com/cosmopolis/dessins/infos");
